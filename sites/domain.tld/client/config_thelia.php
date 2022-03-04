22fec5344c7f7241618cea4b0156290c21ba98ec
Alberttyty <thierry@pixel-plurimedia.fr>
2022-03-04 13:15:10 +0100

    site exemple

---
+++ sites/domain.tld/client/config_thelia.php
@@ -0,0 +1,38 @@
+<?php
+/*************************************************************************************/
+/*                                                                                   */
+/*      Thelia	                                                                     */
+/*                                                                                   */
+/*      Copyright (c) 2005-2013 OpenStudio                                           */
+/*      email : info@thelia.fr                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      This program is free software; you can redistribute it and/or modify         */
+/*      it under the terms of the GNU General Public License as published by         */
+/*      the Free Software Foundation; either version 3 of the License                */
+/*                                                                                   */
+/*      This program is distributed in the hope that it will be useful,              */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU General Public License for more details.                                 */
+/*                                                                                   */
+/*      You should have received a copy of the GNU General Public License            */
+/*      along with this program.  If not, see <http://www.gnu.org/licenses/>.        */
+/*                                                                                   */
+/*************************************************************************************/
+	// Configuration de l'accès à la base de données
+	// ---------------------------------------------
+
+	// Nom de la machine supportant la base de données
+	define('THELIA_BD_HOST', 'localhost');
+
+	// Nom de l'utilisateur de la base de données.
+	define('THELIA_BD_LOGIN', 'root');
+
+	// Mot de passe de l'utilisateur indiqué
+	define('THELIA_BD_PASSWORD', '');
+
+	// Nom de la base à utiliser
+	define('THELIA_BD_NOM', '');
+
+?>
